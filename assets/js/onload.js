81d109506ae790d8c4feb9aa30bb35d9ea082714
Alex <akovalenko777@gmail.com>
2020-05-01 21:24:55 +0300

    add files

---
+++ assets/js/onload.js
@@ -0,0 +1,47 @@
+function addCustomer() {
+    //TODO: добавления покупателя
+}
+
+function viewCustomersList() {
+    //TODO: написать функцию, которая будет выводить список покупателей
+}
+
+function processCustomer() {
+    //TODO: написать функцию, которая будет обрабатывать корзину покупателя
+}
+
+function doneCustomer() {
+    //TODO: функция, которая завершает работу с покупателем: удаляет его и обновляет список покупателей
+}
+
+function addToCart() {
+    //TODO: функция добавления товара в корзину
+}
+
+function setCartQuantity() {
+    //TODO: функция изменения количества товаров в корзине
+}
+
+function removeFromCart() {
+    //TODO: функция удаления товара из корзины
+}
+
+function addProduct() {
+    //TODO: функция добавления товаров в общий список
+}
+
+function calcCart() {
+    //TODO: функция рассчета суммы товаров в корзине
+}
+
+function printReceipt() {
+    //TODO: функция вывода чека, список товаров с суммой, итого
+}
+
+function getCustomersOptions() {
+    //TODO: функция заполнения выпадающего списка покупателей
+}
+
+function getProductsOptions() {
+    //TODO: функция заполнения выпадающего списка товаров
+}
